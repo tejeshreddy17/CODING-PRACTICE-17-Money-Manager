1de74303fd46d7de79f142080a1363aad141cd2b
tejeshreddy17 <tejeshreddy17@gmail.com>
2021-12-01 13:56:35 +0000

    almost-final

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -14,16 +14,15 @@ const TransactionItem = props => {
     <li className="transaction-card">
       <p className="transaction-item-title-style">{title}</p>
       <p className="transaction-item-amount-style">Rs.{amount}</p>
-      <div className="transaction-item-type-delete-icon-container">
-        <p className="transaction-item-type-style">{type}</p>
-        <img
-          className="icon-style"
-          onClick={onclikcingDelete}
-          src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
-          alt="delete"
-          testId="delete"
-        />
-      </div>
+
+      <p className="transaction-item-type-style">{type}</p>
+      <img
+        className="icon-style"
+        onClick={onclikcingDelete}
+        src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+        alt="delete"
+        testId="delete"
+      />
     </li>
   )
 }
